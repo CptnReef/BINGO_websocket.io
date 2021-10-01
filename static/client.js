2b3f3e92255a0d271c006fca613089614f52c4f4
Ben Chan <benchan777@gmail.com>
2021-09-30 20:42:28 -0700

    add bingo number counter & boxes that react to clicking

--- static/client.js
+++ static/client.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
     const socket = io.connect();
 
+    let unavailableNumbers = [];
+    let bingoNumberCounter = 0;
+
+    $('td').on('click', e => {
+        let clickedBoxValue = e.target.innerHTML;
+        let currentBingoNumber = document.getElementById('bingoNumber').innerHTML;
+
+        if (clickedBoxValue == currentBingoNumber) {
+            document.getElementById(e.target.id).style.background = 'pink';
+        };
+    });
+
     const randomNumber = () => {
         // Generate random number from 0 to 149
-        return Math.floor(Math.random() * 150)
+        return Math.floor(Math.random() * 26)
     };
 
     const generateBoard = () => {
         // Keep track of used numbers
         let usedNumbers = [];
 
-        for (let i = 0; i < 24; i ++) {
+        for (let i = 0; i < 24; i++) {
             let newNumber = randomNumber();
 
             // If generated number has already been used, keep generating new numbers
@@ -25,5 +37,27 @@ document.addEventListener('DOMContentLoaded', () => {
         };
     };
 
+    const bingoNumber = () => {
+        setTimeout( () => {
+            bingoNumberCounter ++;
+            let currentNumber = randomNumber();
+
+            while (unavailableNumbers.includes(currentNumber) == true) {
+                currentNumber = randomNumber();
+            };
+
+            unavailableNumbers.push(currentNumber);
+            document.getElementById('bingoNumber').innerHTML = currentNumber
+
+            if (bingoNumberCounter < 26) {
+                bingoNumber();
+            } else {
+                unavailableNumbers = [];
+                return
+            };
+        }, 5000);
+    };
+
     generateBoard();
+    bingoNumber();
 });
